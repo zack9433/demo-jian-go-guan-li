4537752cb53d92e06b034b48338b70c3e9cbcbfa
Zack Yang <zack9433@gmail.com>
2018-08-02 03:19:42 +0800

    ci: modify

--- scripts/version.sh
+++ scripts/version.sh
@@ -4,6 +4,7 @@ RELEASE_TAG=$(yarn release --dry-run | grep "next release version is" | sed 's/.
 
 if [ -n "$RELEASE_TAG" ]
 then
+  echo "Prepare to release version ${RELEASE_TAG}"
   yarn --cwd ./website version ${RELEASE_TAG}
-  yarn --cwd ./website publish-gh-pages
+  GIT_USER="${GH_NAME}" yarn --cwd ./website publish-gh-pages
 fi
