d7ba768ee669ba8826ec8e5d5ceadf3c72fa9c5c
Zack Yang <zack9433@gmail.com>
2018-08-02 03:58:12 +0800

    fix: bug (#11)
    * feat: add feature 1
    
    * feat: add feature 2 (#2)
    
    * ci: modify
    
    * ci: add branch
    
    * feat: add feature 3 (#6)
    
    * feat: add feature 4 (#8)
    
    * fix: bug (#10)

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,6 +5,6 @@ RELEASE_TAG=$(yarn release --dry-run | grep "next release version is" | sed 's/.
 if [ -n "$RELEASE_TAG" ]
 then
   echo "Prepare to release version ${RELEASE_TAG}"
-  yarn --cwd ./website version ${RELEASE_TAG}
+  yarn --cwd ./website run version ${RELEASE_TAG}
   GIT_USER="${GH_NAME}" CURRENT_BRANCH=master yarn --cwd ./website publish-gh-pages
 fi
