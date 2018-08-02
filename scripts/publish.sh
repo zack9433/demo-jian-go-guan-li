24c9776a7276ba6501b6222b433c7b7c8b41011b
Zack Yang <zack9433@gmail.com>
2018-08-02 20:41:38 +0800

    refactor: modify

--- scripts/publish.sh
+++
@@ -1,5 +0,0 @@
-# git config --global user.name "${GH_NAME}"
-# git config --global user.email "${GH_EMAIL}"
-echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
-GIT_USER="${GH_NAME}" CURRENT_BRANCH=master yarn --cwd ./website publish-gh-pages
-yarn travis-deploy-once "yarn release"
